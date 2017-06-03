d90e772fef35420f46ada7f663f885a3a45d13b8
Caleb Braun <Caleb@Caleb-Brauns-MacBook.local>
2017-06-03 15:44:42 -0500

    made filtering not switch to boxplot

--- readability.js
+++ readability.js
@@ -49,7 +49,7 @@ d3.csv(csvFile, function(error, csv_data) {
     });
 
     data = csv_data;
-    update();
+    init(data);
 });
 
 
@@ -68,17 +68,9 @@ function update() {
     // Filter out unchecked categories
     for (var i = 0; i < uncheckedCategories.length; i++) {
         newData = newData.filter(function(d) {
-
-            // format the data into JSON
-            // var x = d["categories"].replace(/\', \'/g, '", "');
-            // x = x.replace(/\[\'/g, '["');
-            // x = x.replace(/\'\]/g, '"]');
-            // x = x.replace(/\", \'/g, '", "');
-            // x = x.replace(/\', \"/g, '", "');
             var x = d["categories"];
             if (x.length == 0) { return true; }
-            // End format
-            // console.log(x);
+
             var categories = JSON.parse(x);
             for (var i = 0; i < categories.length; i++) {
                 if (uncheckedCategories.indexOf(categories[i]) > -1) {
@@ -90,9 +82,28 @@ function update() {
     }
 
     d3.select(".inner-wrapper").remove();
+    var showViolin = chart1.violinPlots.options.show;
+    var showBean = chart1.dataPlots.options.showBeanLines;
+    var showScatter = chart1.dataPlots.options.showPlot;
+    var showTrends = chart1.dataPlots.options.showLines;
+    init(newData);
 
+    if (showTrends) handleTrendLinesButton();
+    if (showBean) {
+        handleBeanPlotButton();
+    } else if (showScatter) {
+        handleScatterPlotButton();
+    } else if (showViolin) {
+        handleViolinPlotButton();
+    } else {
+        handleBoxPlotButton();
+    }
+
+}
+
+function init(newData) {
     chart1 = makeDistroChart({
-        data: newData,
+        data:newData,
         xName:'state',
         yName:'coleman_liau_index',
         axisLabels: {xAxis: 'State', yAxis: 'Values'},
@@ -103,47 +114,21 @@ function update() {
     chart1.renderDataPlots();
     chart1.renderNotchBoxes({showNotchBox:false});
     chart1.renderViolinPlot({showViolinPlot:false});
-
-    // newItems = svg.selectAll("circle").data(newData);
-    //
-    // newItems.enter()
-    //     .append('svg:circle')
-    //     .attr('r', function(d) { return 2 }) //return d["useful"]; })
-    //     .attr('cx', function(d) {
-    //         var variance = parseInt(Math.abs(d["flesch_reading_ease"] - 50)) * Math.random();
-    //         return (d["stars"]) * 110 + variance;
-    //     })
-    //     .attr('cy', function(d) { return (d["flesch_reading_ease"] * 5 - 150); })
-    //     .style('fill', 'black');
-    //
-    // newItems.exit()
-    //     .remove();
-}
-
-function makeValidSelector(name) {
-    valid = name.replace(/\s/g, '-');
-    valid = valid.replace(/\&/g, 'and');
-    return valid;
 }
 
 function initBusinessFilters() {
-    for (var i = 0; i < businessTypes.length; i++) {
-        d3.select(".businesses").append('label')
-            .attr('id', 'label'+i)
-            .append('input')
-            .attr('type', 'checkbox')
-            .attr('id', makeValidSelector(businessTypes[i]))
-            // .attr('checked', 'true')
-            .on('change', update);
-        d3.select('#label'+i).append('span')
-            .html(businessTypes[i]);
-    }
-}
+    var businessDiv = d3.select(".businesses");
+    businessDiv.append('label')
+        .attr('id', 'selectAllBusinesses')
+        .append('input')
+        .attr('type', 'checkbox')
+        .on('change', checkAll(businessDiv));
 
+    d3.select('#selectAllBusinesses').append('span')
+        .html('Select All');
 
-function initBusinessFilters() {
     for (var i = 0; i < businessTypes.length; i++) {
-        d3.select(".businesses").append('label')
+        businessDiv.append('label')
             .attr('id', 'label'+i)
             .append('input')
             .attr('type', 'checkbox')
@@ -156,7 +141,7 @@ function initBusinessFilters() {
 }
 
 function initCityFilters() {
-    var states = ['PA','NC','IL','AZ','NV','WI','OH'];
+    var states = ['NC','OH','NV','WI','AZ','PA','IL'];
     var cityDiv = d3.select(".cities");
     for (var i = 0; i < states.length; i++) {
         cityName = convertStateToCity(states[i])
@@ -171,17 +156,27 @@ function initCityFilters() {
     }
 }
 
+function checkAll(div) {
+    div.selectAll('input').attr('checked', 'true');
+}
+
 
 function convertStateToCity(state) {
     var stateToCity = {
-        'PA' : 'Pittsburgh',
         'NC' : 'Charlotte',
-        'IL' : 'Urbana-Champaign',
-        'AZ' : 'Phoenix',
+        'OH' : 'Cleveland',
         'NV' : 'Las Vegas',
         'WI' : 'Madison',
-        'OH' : 'Cleveland'
+        'AZ' : 'Phoenix',
+        'PA' : 'Pittsburgh',
+        'IL' : 'Urbana-Champaign'
     }
     return stateToCity[state];
+}
+
+function makeValidSelector(name) {
+    valid = name.replace(/\s/g, '-');
+    valid = valid.replace(/\&/g, 'and');
+    return valid;
 }
 //
